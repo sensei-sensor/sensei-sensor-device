c3877f5421b98b22dd563d68182b0914f812a4a3
Naoki Takahashi <aho.mickey.mein@gmail.com>
2020-11-29 22:10:29 +0900

    autopep8の導入とサンプルコードの追加

--- sample.py
+++ sample.py
@@ -1,19 +1,29 @@
-from bluepy.btle import Scanner, DefaultDelegate
+# https://www.ipride.co.jp/blog/2510 このサンプルコード
 
-class ScanDelegate(DefaultDelegate):
-		def __init__(self):
-				DefaultDelegate.__init__(self)
+from bluepy import btle
 
-		def handleDiscovery(self, dev, isNewDev, isNewData):
-				if isNewDev:
-						print("Discovered device", dev.addr)
-				elif isNewData:
-						print("Received new data from", dev.addr)
+# デバイスをスキャンするためのクラスを初期化する。
+# 引数(index=0)は、使用するBluetoothインターフェースの番号を表す
+# ※ index=0 が /dev/hci0 に対応する
+scanner = btle.Scanner(0)
 
-scanner = Scanner().withDelegate(ScanDelegate())
-devices = scanner.scan(10.0)
+# デバイスをスキャンする(結果はValuesView[ScanEntry]型で返される)
+# 引数(timeout=10.0)は、スキャンする秒数を表す
+devices = scanner.scan(3.0)
 
-for dev in devices:
-		print ("Device %s (%s), RSSI=%d dB") % (dev.addr, dev.addrType, dev.rssi)
-		for (adtype, desc, value) in dev.getScanData():
-				print ("  %s = %s" )% (desc, value)
+# スキャンした結果を表示
+# ・ペリフェラルデバイスとの接続にはMACアドレスとアドレスタイプが必要
+# ・RSSIは要するに電波強度で、それなりにないと接続に成功しない
+# ・アドバタイシングデータは、そのペリフェラルデバイスがアドバタイズパケットで
+# 　周囲に発信している、デバイスの情報を表すデータ
+for device in devices:
+    print(f'MACアドレス：{device.addr}')
+    print(f'  アドレスタイプ：{device.addrType}')
+    print(f'  RSSI：{device.rssi}')
+
+    # adTypeCodeはアドバタイシングデータのキーで、
+    # descriptionはそれを人間が読めるように翻訳したもの。
+    # そしてvalueTextはアドバタイシングデータの値
+    print(f'  アドバタイシングデータ：')
+    for (adTypeCode, description, valueText) in device.getScanData():
+        print(f'    {description}：{valueText}')
