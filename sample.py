7e800aab211c2a3a320ce4692c367e6939f681eb
Naoki Takahashi <aho.mickey.mein@gmail.com>
2020-12-05 22:54:01 +0900

    サンプルコードの編集をした

--- sample.py
+++ sample.py
@@ -11,19 +11,27 @@ scanner = btle.Scanner(0)
 # 引数(timeout=10.0)は、スキャンする秒数を表す
 devices = scanner.scan(3.0)
 
-# スキャンした結果を表示
-#  ペリフェラルデバイスとの接続にはMACアドレスとアドレスタイプが必要
-#  RSSIは要するに電波強度で、それなりにないと接続に成功しない
-#  アドバタイシングデータは、そのペリフェラルデバイスがアドバタイズパケットで
-#  周囲に発信している、デバイスの情報を表すデータ
-for device in devices:
-    print(f'MACアドレス：{device.addr}')
-    print(f'  アドレスタイプ：{device.addrType}')
-    print(f'  RSSI：{device.rssi}')
+# # スキャンした結果を表示
+# #  ペリフェラルデバイスとの接続にはMACアドレスとアドレスタイプが必要
+# #  RSSIは要するに電波強度で、それなりにないと接続に成功しない
+# #  アドバタイシングデータは、そのペリフェラルデバイスがアドバタイズパケットで
+# #  周囲に発信している、デバイスの情報を表すデータ
+# device = devices
+# print(device, end='\n')
+# print(type(device))\
+connectable_devices = [device for device in list(devices) if device.connectable]
+for device in connectable_devices:
+    print(f'MAC Address:    {device.addr}')
+    print(f'  Address Type: {device.addrType}')
+    print(f'  iface:        {device.iface}')
+    print(f'  RSSI:         {device.rssi}')
+    print(f'  Connectable?: {device.connectable}')
+    print(f'  Update Count: {device.updateCount}')
+    
 
-    # adTypeCodeはアドバタイシングデータのキーで、
-    # descriptionはそれを人間が読めるように翻訳したもの。
-    # そしてvalueTextはアドバタイシングデータの値
-    print(f'  アドバタイシングデータ：')
-    for (adTypeCode, description, valueText) in device.getScanData():
-        print(f'    {description}：{valueText}')
+# adTypeCodeはアドバタイシングデータのキーで、
+# descriptionはそれを人間が読めるように翻訳したもの。
+# そしてvalueTextはアドバタイシングデータの値
+    # print(f'  アドバタイシングデータ：')
+    # for (adTypeCode, description, valueText) in device.getScanData():
+    #     print(f'    {description}：{valueText}')
