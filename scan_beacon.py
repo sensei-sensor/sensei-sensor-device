c2607d9e88bdeef559342c815026a05ba9316f52
Yoshidas Lab Raspberry Pi <1176731@st.anan-nct.ac.jp>
2021-07-02 11:56:04 +0900

    send_serverを再開

--- scan_beacon.py
+++ scan_beacon.py
@@ -1,12 +1,11 @@
 import time
 import send_server
-import read_json
 from beacontools import BeaconScanner, IBeaconFilter, IBeaconAdvertisement
 
 
 def callback(bt_addr, rssi, packet, additional_info):
     print("MAC Address: %s \n RSSI: %d" % (bt_addr, rssi))
-    read_json.check_tag_number(bt_addr)
+    send_server.send_server(bt_addr)
 
 
 # scan for all iBeacon advertisements from beacons with certain properties:
