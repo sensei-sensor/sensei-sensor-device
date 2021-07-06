995cc563f0c1784bf5ae2db207c5bfdafab6c5e9
Yoshidas Lab Raspberry Pi <1176731@st.anan-nct.ac.jp>
2021-07-06 13:58:11 +0900

    デバッグプログラムの追加

--- debug.py
+++ debug.py
@@ -2,11 +2,18 @@ import time
 import read_json
 from beacontools import BeaconScanner, IBeaconFilter, IBeaconAdvertisement
 
+tag0 = 0
+tag1 = 0
 
 def callback(bt_addr, rssi, packet, additional_info):
-    if (read_json.check_tag_number(bt_addr) != None):
+    global tag0, tag1
+    if (read_json.check_tag_number(bt_addr) is not None):
         print("Tag number: %s" % (read_json.check_tag_number(bt_addr)))
         print(" RSSI: %d" % (rssi))
+        if (read_json.check_tag_number(bt_addr) == 0):
+            tag0 += 1
+        elif (read_json.check_tag_number(bt_addr) == 1):
+            tag1 += 1
 
 
 # scan for all iBeacon advertisements from beacons with certain properties:
@@ -21,3 +28,7 @@ scanner = BeaconScanner(callback,
 scanner.start()
 time.sleep(5)
 scanner.stop()
+
+print()
+print("Tag0: " + str(tag0))
+print("Tag1: " + str(tag1))
